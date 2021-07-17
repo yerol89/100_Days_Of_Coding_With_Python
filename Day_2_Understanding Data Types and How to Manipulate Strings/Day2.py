d1deb824f6d63e07aefb2569ce42aa1426f3084f
yunustalip_erol <yunustalip.erol@kbyazilim.com.tr>
2021-07-17 17:11:23 +0300

    New Functions to Add

--- Day_2_Understanding Data Types and How to Manipulate Strings/Day2.py
+++ Day_2_Understanding Data Types and How to Manipulate Strings/Day2.py
@@ -12,3 +12,27 @@ first_digit = two_digit_number[0]
 second_digit = two_digit_number[1]
 result = int(first_digit) + int(second_digit)
 print("Addition of " + two_digit_number + "'s digits is " + str(result))
+
+# Calculate BMI Value
+weight = input("What is your weight in Kg.\n")
+height = input("What is your height in m.\n")
+weight = float(weight)
+height = float(height)
+BMI = weight/(height * height)
+print("Your BMI is " + str(BMI))
+
+# f_String => We can use many primitive data types with f_string
+score = input("Your Score:\n")
+height2 = input("Your height:\n")
+married = input("Marrial Status:\n")
+print(f"Your score is {score}, your height is {height} and marial status is {married}")
+
+# Calculate rest of your life in days, months and weeks assume you will live until 90
+age = input("How old are you?\n")
+left = 90 - int(age)
+days = left * 365
+weeks = left * 52
+months = left * 12
+print(f"You have {days} days, {weeks} weeks or {months} months to live")
+
+
